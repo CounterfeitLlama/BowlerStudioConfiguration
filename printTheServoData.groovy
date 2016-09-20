3a70215a899c1a63a64344b223bbb682cef9bf1e
Andrew Turley <aturley123@gmail.com>
2016-09-20 11:45:44 -0400

    Testing getting data about vitamins

--- printTheServoData.groovy
+++ printTheServoData.groovy
@@ -1,1 +1,10 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import eu.mihosoft.vrl.v3d.parametrics.*;
+
+HashMap<String, Object>  vitaminData = Vitamins.getConfiguration("hobbyServo", "towerProMG91")
+
+println vitaminData
+
+double tipOfShaftToBottomOfFlange = vitaminData.tipOfShaftToBottomOfFlange
+
+println tipOfShaftToBottomOfFlange
