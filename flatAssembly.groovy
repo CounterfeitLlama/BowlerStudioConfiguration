7ebc8deef0c7fcfc35be22fd52493e7b7e6ff47e
Andrew Turley <aturley123@gmail.com>
2016-09-22 08:20:54 -0400

    Created file

--- flatAssembly.groovy
+++ flatAssembly.groovy
@@ -1,1 +1,14 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.parametrics.*;
+
+CSG makePart() {
+	LengthParameter thickness = new LengthParameter("Material Thickness",3.15,[10,1]);
+
+	CSG cube = new Cube(40, 60, thickness.getMM()).toCSG()
+	cube.setParameter(thickness)
+	cube.setRegenerate({
+		return makePart()
+	})
+	return cube
+}
+
+return makePart()
